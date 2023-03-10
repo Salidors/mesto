bc5ca180a4d1ad570c9b01fe84068c39fd5027ab
Abramova Nina <abramova.nina.g@gmail.com>
2023-03-10 21:31:55 +0300

    feat: adding functionality, fixes and script

--- script/index.js
+++ script/index.js
@@ -1,24 +1,52 @@
-console.log("kryakozyabra");
+/*============== открыть или закрыть попап ==================*/
 
 const popupElement = document.querySelector(".popup");
 const popupCloseButtonElement = popupElement.querySelector(".popup__close");
-const popupOpenButtonElement = document.querySelector(".profile__add-button");
-
-const openPopup = function () {
-    popupElement.classList.add("popup__is-opened");
-};
+const popupOpenButtonElement = document.querySelector(".profile__button");
 
 const closePopup = function () {
     popupElement.classList.remove("popup__is-opened");
 };
 
-const closePopupByClickOnOverlay = function (event) {
-    console.log(event.target, event.currentTarget);
-    closePopupByClickOnOverlay();
+const openPopup = function () {
+    popupElement.classList.add("popup__is-opened");
 };
 
-popupOpenButtonElement.addEventListener("click", openPopup);
+
+const closePopupByClickOnOverlay = function (event) {
+    if (event.target === event.currentTarget) {
+        closePopup();
+    }
+};
 
 popupCloseButtonElement.addEventListener("click", closePopup);
+popupOpenButtonElement.addEventListener("click", openPopup);
+
 
 popupElement.addEventListener("click", closePopupByClickOnOverlay);
+
+
+
+/* ================ добавить данные ===============*/
+
+const addButton = document.querySelector('.popup__save');
+
+function addForm(event) {
+    console.log(event);
+    let name = document.querySelector('.popup__subtitle-name');
+    let info = document.querySelector('.popup__subtitle-info');
+
+    let profileName = document.querySelector('.profile__name');
+    let profileTitle = document.querySelector('.profile__title');
+
+    profileName.textContent = name.value;
+    profileTitle.textContent = info.value;
+
+
+    name.value = '';
+    info.value = '';
+
+    closePopup();
+}
+
+addButton.addEventListener('click', addForm);
